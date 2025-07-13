435c5c6464cfb281463011f651b79c49d06e6f96
SayedimanMostafavi <i.mostafavi1988@gmail.com>
2025-07-13 14:25:06 +0000

    initial commit

---
+++ P2_ASCII_art_generator1.py
@@ -0,0 +1,39 @@
+# Problem number 2
+# Title: ASCII Art Generator 1
+# Question: Ask for the number of rows from user and write a Python code to generate this pattern  
+# Example Output: input: 4 ----------- output: 
+#											   * - * - * -
+#      										   * - * - * -
+#     										   * - * - * -
+#     										   * - * - * -
+
+
+
+
+
+# Short Solution:
+
+try:
+	print("\n".join(["* - * - * - "]*abs(int(input("Enter the number of rows: ").strip()))))
+		
+		
+except ValueError:
+	print("Input Type Error !!!")
+	
+	
+
+
+	
+"""
+# Classical Solution (Nested Loop)
+
+try:
+	number_of_rows = int(input("Enter the number of rows: ").strip()) # Get the number of rows from user
+	for row in range(number_of_rows):       					      # First for-loop handles the rows 
+		for column in range(3):                						  # Second for-loop handles the columns 
+			print("* -",end=" ")               						  # Printing pattern for each two-column (* -) without going to next line 
+		print()                              						  # Move to next line for the next row
+			
+except ValueError:
+	print("Input Type Error")
+"""
--- README.md
+++ README.md
@@ -5,10 +5,18 @@
 Prerequisites: Python Syntax , Programming Basics
 Level: Mixed (intermediate to advance)
 
-I am a third year software engineer who is eager to 
-learn Python for AI purposes. I studied basic syntax
-of Python. Now I decided to broaden my knowledge through
-solving problems . I will also post problems to my Github 
-repository for those who are curious to challenge their 
-programming knowledge.
+I started learning programming in unuversity with C , then Java and C++.
+But when I tried Python , I fell in love with its simple, clear, readable syntax.
+So I decided to review some problems to broaden my knowledge in this valuable language.
+If you have already studied programming (in other languages) just study basic syntax
+of Python and join me in this journey. 
+
+I will push problems and their solutions time to time after practicing them.
+
+If you had any suggestion or question , feel free to contact me through Email.
+Email: i.mostafavi1988@gmail.com
+
+Thank you in advance,
+
+Sayediman Mostafavi, 
 
